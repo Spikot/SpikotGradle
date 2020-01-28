a63a54ec79017f8dbe6935774bd359405d496ebf
HeartPattern <jun2620354@kaist.ac.kr>
2020-01-29 03:06:27 +0900

    Add spikot extension method

--- src/main/kotlin/kr/heartpattern/spikotgradle/SpikotExtension.kt
+++ src/main/kotlin/kr/heartpattern/spikotgradle/SpikotExtension.kt
@@ -1,5 +1,13 @@
 package kr.heartpattern.spikotgradle
 
-open class SpikotExtension{
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.get
+
+fun Project.spikot(configuration: SpikotExtension.() -> Unit) {
+    val extension = extensions["spikot"] as SpikotExtension
+    extension.configuration()
+}
+
+open class SpikotExtension {
     val output: String? = null
 }
